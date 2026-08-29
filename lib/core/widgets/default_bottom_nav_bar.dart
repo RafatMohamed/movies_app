3a3efdd330c16d2e3f68b8aad8463672071b5221
ahmed.elnoby <ahmed.ellnoby13@gmail.com>
2026-08-29 03:46:36 +0300

    fix nav bar navigation experence & delete useless platforms files

--- lib/core/widgets/default_bottom_nav_bar.dart
+++ lib/core/widgets/default_bottom_nav_bar.dart
@@ -6,22 +6,20 @@ import 'package:movies_app/core/utilities/app_padding.dart';
 import 'package:svg_flutter/svg.dart';
 
 class DefaultBottomNavigationBar extends StatelessWidget {
-  const DefaultBottomNavigationBar({super.key,required this.currentIndex, required this.onCurrentIndexChange});
-  final int currentIndex ;
+  const DefaultBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onCurrentIndexChange,
+  });
+  final int currentIndex;
   final Function(int index) onCurrentIndexChange;
   @override
   Widget build(BuildContext context) {
     List<BottomNavigationBarModel> itemsNav = [
+      BottomNavigationBarModel(iconPath: AppAssets.homeIcon, label: "Home"),
+      BottomNavigationBarModel(iconPath: AppAssets.searchIcon, label: 'Search'),
       BottomNavigationBarModel(
-        iconPath:AppAssets.homeIcon,
-        label: "Home",
-      ),
-      BottomNavigationBarModel(
-        iconPath: AppAssets.searchIcon,
-        label: 'Search',
-      ),
-      BottomNavigationBarModel(
-        iconPath:  AppAssets.exploreIcon,
+        iconPath: AppAssets.exploreIcon,
         label: "Explore",
       ),
       BottomNavigationBarModel(
@@ -33,47 +31,33 @@ class DefaultBottomNavigationBar extends StatelessWidget {
       borderRadius: BorderRadius.circular(AppBorderRadius.r20),
       child: Container(
         height: 70,
-        padding:  const EdgeInsets.symmetric(horizontal: AppPadding.p24),
-        decoration:  ShapeDecoration(
-          gradient: LinearGradient(colors: [
-            AppColors.deepBlack.withValues(
-                alpha: 0.5
-            ),AppColors.lightBlack.withValues(
-                alpha: 0.5
-            ),AppColors.white.withValues(
-                alpha: 0.2
-            ),
-          ],
-            begin: AlignmentDirectional.topStart,
-            end: AlignmentDirectional.bottomEnd,
-          ),
-          shadows:  [
-            BoxShadow(
-              color: AppColors.deepBlack.withValues(alpha: 0.05),
-            ),
-          ],
-          shape: const Border(),
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: AppPadding.p24),
+        decoration: const BoxDecoration(color: AppColors.lightBlack),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:
-          itemsNav.map((item) {
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: itemsNav.map((item) {
             final index = itemsNav.indexOf(item);
             final isSelected = index == currentIndex;
-            return GestureDetector(
-              onTap: () {
-               onCurrentIndexChange(index);
-              },
-              child: AnimatedContainer(
-                curve: Curves.linear,
-                duration: const Duration(milliseconds: 50),
-                alignment: AlignmentDirectional.center,
-                child:SvgPicture.asset(
-                  itemsNav[index].iconPath,
-                  fit: .scaleDown,
-                  width: 30,
-                  height: 30,
-                  colorFilter: ColorFilter.mode(isSelected?AppColors.gold:AppColors.white, .srcIn),
+            return Expanded(
+              child: GestureDetector(
+                behavior: .opaque,
+                onTap: () {
+                  onCurrentIndexChange(index);
+                },
+                child: AnimatedContainer(
+                  curve: Curves.linear,
+                  duration: const Duration(milliseconds: 50),
+                  alignment: AlignmentDirectional.center,
+                  child: SvgPicture.asset(
+                    itemsNav[index].iconPath,
+                    fit: .scaleDown,
+                    width: 30,
+                    height: 30,
+                    colorFilter: ColorFilter.mode(
+                      isSelected ? AppColors.gold : AppColors.white,
+                      .srcIn,
+                    ),
+                  ),
                 ),
               ),
             );
@@ -88,8 +72,5 @@ class BottomNavigationBarModel {
   final String label;
   final String iconPath;
 
-  BottomNavigationBarModel({
-    required this.label,
-    required this.iconPath,
-  });
+  BottomNavigationBarModel({required this.label, required this.iconPath});
 }
