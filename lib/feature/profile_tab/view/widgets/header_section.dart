61ee16b57ee0a6fb81c17a13f1673497e238762f
Raafat <raafatm2001@gmail.com>
2026-09-15 14:06:12 +0300

    +add :header profile tap

--- lib/feature/profile_tab/view/widgets/header_section.dart
+++ lib/feature/profile_tab/view/widgets/header_section.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gap/flutter_gap.dart';
+import 'package:movies_app/core/cubit/history_list_cubit/history_list_cubit.dart';
+import 'package:movies_app/core/cubit/watch_list_cubit/watch_list_cubit/watch_list_cubit.dart';
 import 'package:movies_app/core/models/img_profile_model.dart';
 import 'package:movies_app/core/utilities/app_colors.dart';
 import 'package:movies_app/core/utilities/app_padding.dart';
+import 'package:movies_app/core/utilities/auth/auth_cubit.dart';
 import 'package:svg_flutter/svg.dart';
 import 'package:movies_app/l10n/generated/app_localizations.dart';
+import '../../../../core/cubit/history_list_cubit/history_list_state.dart';
+import '../../../../core/utilities/auth/auth_state.dart';
 
 class HeaderSection extends StatelessWidget {
   const HeaderSection({super.key});
+
   @override
   Widget build(BuildContext context) {
     final String imageProfile = ImgProfileModel.avatars.first.imgPath;
@@ -34,12 +41,23 @@ class HeaderSection extends StatelessWidget {
                   ),
                 ),
                 const Gap(16),
-                const Expanded(
+               Expanded(
                   flex: 3,
                   child: FittedBox(
-                    child: Text(
-                      'Ahmed elnoby',
-                      style: TextStyle(color: AppColors.white),
+                    child: BlocBuilder<AuthCubit, AuthState>(
+                      builder: (context, state) {
+                        final String? name = state is Authenticated
+                            ? state.user.displayName
+                            : "UnKnown";
+                        return Text(
+                          name??"UnKnown",
+                          style: const TextStyle(
+                            color: AppColors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: .center,
+                        );
+                      },
                     ),
                   ),
                 ),
@@ -57,15 +75,23 @@ class HeaderSection extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: .center,
                       children: [
-                        const Expanded(
+                        Expanded(
                           flex: 16,
                           child: FittedBox(
-                            child: Text(
-                              '12',
-                              style: TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
+                            child: BlocBuilder<WatchListCubit, WatchListState>(
+                              builder: (context, state) {
+                                final count = state is WatchListSuccess
+                                    ? state.movies.length
+                                    : 0;
+                                return Text(
+                                  "$count",
+                                  style: const TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: .center,
+                                );
+                              },
                             ),
                           ),
                         ),
@@ -87,17 +113,25 @@ class HeaderSection extends StatelessWidget {
                   Expanded(
                     flex: 5,
                     child: Column(
-                      crossAxisAlignment: .end,
+                      crossAxisAlignment: .center,
                       children: [
-                        const Expanded(
+                        Expanded(
                           flex: 16,
                           child: FittedBox(
-                            child: Text(
-                              '10',
-                              style: TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
+                            child: BlocBuilder<HistoryCubit, HistoryState>(
+                              builder: (context, state) {
+                                final count = state is HistorySuccess
+                                    ? state.movies.length
+                                    : 0;
+                                return Text(
+                                  "$count",
+                                  style: const TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: .center,
+                                );
+                              },
                             ),
                           ),
                         ),
