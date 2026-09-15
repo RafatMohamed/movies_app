40176ceb9b7f2a8442619f5fa927cf2eeee73fb0
Nadeen Gebril <nadeengebril82@gmail.com>
2026-09-15 16:58:44 +0300

    Add profile header logic

--- lib/feature/profile_tab/view/widgets/header_section.dart
+++ lib/feature/profile_tab/view/widgets/header_section.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gap/flutter_gap.dart';
 import 'package:movies_app/core/models/img_profile_model.dart';
+import 'package:movies_app/core/models/user_model.dart';
 import 'package:movies_app/core/utilities/app_colors.dart';
 import 'package:movies_app/core/utilities/app_padding.dart';
+import 'package:movies_app/core/utilities/auth/auth_cubit.dart';
 import 'package:svg_flutter/svg.dart';
 import 'package:movies_app/l10n/generated/app_localizations.dart';
 
@@ -10,118 +13,133 @@ class HeaderSection extends StatelessWidget {
   const HeaderSection({super.key});
   @override
   Widget build(BuildContext context) {
-    final String imageProfile = ImgProfileModel.avatars.first.imgPath;
+    // final String imageProfile = ImgProfileModel.avatars.first.imgPath;
     final AppLocalizations l10n = AppLocalizations.of(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: AppPadding.p16),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 8,
-            child: Column(
-              crossAxisAlignment: .center,
-              mainAxisSize: .min,
-              children: [
-                Expanded(
-                  flex: 10,
-                  child: FittedBox(
-                    child: SvgPicture.asset(
-                      imageProfile,
-                      height: 118,
-                      width: 118,
-                      fit: .fill,
+    final authCubit = context.read<AuthCubit>();
+    return StreamBuilder<UserModel?>(
+      stream: authCubit.watchCurrentUserData(),
+      builder: (context, snapshot) {
+        final UserModel? user = snapshot.data;
+
+        final avatars = ImgProfileModel.avatars;
+        final int avatarIndex = (user?.avatarIndex ?? 0).clamp(
+          0,
+          avatars.length - 1,
+        );
+        final String imageProfile = avatars[avatarIndex].imgPath;
+        final String name = user?.name ?? '';
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppPadding.p16),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 8,
+                child: Column(
+                  crossAxisAlignment: .center,
+                  mainAxisSize: .min,
+                  children: [
+                    Expanded(
+                      flex: 10,
+                      child: FittedBox(
+                        child: SvgPicture.asset(
+                          imageProfile,
+                          height: 118,
+                          width: 118,
+                          fit: .fill,
+                        ),
+                      ),
                     ),
-                  ),
+                    const Gap(16),
+                    Expanded(
+                      flex: 3,
+                      child: FittedBox(
+                        child: Text(
+                          name,
+                          style: TextStyle(color: AppColors.white),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                const Gap(16),
-                const Expanded(
-                  flex: 3,
-                  child: FittedBox(
-                    child: Text(
-                      'Ahmed elnoby',
-                      style: TextStyle(color: AppColors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 14,
-            child: Padding(
-              padding: const EdgeInsets.all(26),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: Column(
-                      crossAxisAlignment: .center,
-                      children: [
-                        const Expanded(
-                          flex: 16,
-                          child: FittedBox(
-                            child: Text(
-                              '12',
-                              style: TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 6,
-                          child: FittedBox(
-                            child: Text(
-                              l10n.watchList,
-                              style: const TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Column(
-                      crossAxisAlignment: .end,
-                      children: [
-                        const Expanded(
-                          flex: 16,
-                          child: FittedBox(
-                            child: Text(
-                              '10',
-                              style: TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 6,
-                          child: FittedBox(
-                            child: Text(
-                              l10n.history,
-                              style: const TextStyle(
-                                color: AppColors.white,
-                                fontWeight: .bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
               ),
-            ),
+              Expanded(
+                flex: 14,
+                child: Padding(
+                  padding: const EdgeInsets.all(26),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Column(
+                          crossAxisAlignment: .center,
+                          children: [
+                            const Expanded(
+                              flex: 16,
+                              child: FittedBox(
+                                child: Text(
+                                  '12',
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: .bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 6,
+                              child: FittedBox(
+                                child: Text(
+                                  l10n.watchList,
+                                  style: const TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: .bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Column(
+                          crossAxisAlignment: .end,
+                          children: [
+                            const Expanded(
+                              flex: 16,
+                              child: FittedBox(
+                                child: Text(
+                                  '10',
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: .bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 6,
+                              child: FittedBox(
+                                child: Text(
+                                  l10n.history,
+                                  style: const TextStyle(
+                                    color: AppColors.white,
+                                    fontWeight: .bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
